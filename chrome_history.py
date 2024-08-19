ef2bd3fd5b2c55eadd1054e4b20744c4108bd3db
Joseph Merigala <joseph.merigala@gmail.com>
2024-08-19 12:25:47 +0530

    V0.5.5 fixed->parsed urls table

--- chrome_history.py
+++ chrome_history.py
@@ -1,15 +1,40 @@
 import os
 import sqlite3
+import datetime
 
-db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
-connection = sqlite3.connect(db)
-cursor = connection.cursor()
-"""
-cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
-for table in cursor.fetchall():
-    print(table)
-"""
-cursor.execute("SELECT * FROM urls;")
-for line in cursor.fetchall():
-    print(line)
-connection.close()
+def get_tables():
+    db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
+    for table in cursor.fetchall():
+        try:
+            print(table)
+        except:
+            continue
+    connection.close()
+
+def date_from_webkit(timestamp):
+    epoch_start = datetime.datetime(1601,1,1)
+    delta = datetime.timedelta(microseconds=int(timestamp))
+    print(epoch_start+delta)
+
+def ordered_url_history():
+    q = "SELECT * FROM urls ORDER BY last_visit_time DESC;"
+    db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    cursor.execute("SELECT sql from sqlite_schema where name='urls';")
+    print(cursor.fetchall())
+    cursor.execute(q)
+    data =  cursor.fetchall()
+    for line in data:
+        try:
+            date_from_webkit(int(line[5]))
+            print(line,"\n","--"*50)
+        except:
+            continue
+
+    connection.close()
+
+get_tables()
