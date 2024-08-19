bc996ac641e2044da984c1e042f3355b51db39e3
Joseph Merigala <joseph.merigala@gmail.com>
2024-08-19 12:39:53 +0530

    V0.5.5.1 added->visited_links_ordered_by_count

--- chrome_history.py
+++ chrome_history.py
@@ -2,10 +2,11 @@ import os
 import sqlite3
 import datetime
 
+db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
+connection = sqlite3.connect(db)
+cursor = connection.cursor()
+
 def get_tables():
-    db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
-    connection = sqlite3.connect(db)
-    cursor = connection.cursor()
     cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
     for table in cursor.fetchall():
         try:
@@ -13,17 +14,12 @@ def get_tables():
         except:
             continue
     connection.close()
-
 def date_from_webkit(timestamp):
     epoch_start = datetime.datetime(1601,1,1)
     delta = datetime.timedelta(microseconds=int(timestamp))
     print(epoch_start+delta)
-
 def ordered_url_history():
     q = "SELECT * FROM urls ORDER BY last_visit_time DESC;"
-    db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
-    connection = sqlite3.connect(db)
-    cursor = connection.cursor()
     cursor.execute("SELECT sql from sqlite_schema where name='urls';")
     print(cursor.fetchall())
     cursor.execute(q)
@@ -35,6 +31,17 @@ def ordered_url_history():
         except:
             continue
 
-    connection.close()
 
-get_tables()
+def visited_links_ordered_by_count():
+    q = "SELECT * FROM sqlite_schema WHERE name='visited_links';"
+    cursor.execute(q)
+    print(cursor.fetchall())
+    q = "SELECT * FROM visited_links ORDER BY visit_count DESC;"
+    cursor.execute(q)
+    for line in cursor.fetchall():
+        try:
+            print(line)
+        except:
+            continue
+
+connection.close()
