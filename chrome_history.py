235dba7067b2fd8b2286e106b4644d15ea17900f
Joseph Merigala <joseph.merigala@gmail.com>
2024-08-22 08:43:00 +0530

    V0.7.4 - Merged scripts accessing history.db

--- chrome_history.py
+++ chrome_history.py
@@ -2,46 +2,52 @@ import os
 import sqlite3
 import datetime
 
-db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
-connection = sqlite3.connect(db)
-cursor = connection.cursor()
 
-def get_tables():
-    cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
-    for table in cursor.fetchall():
-        try:
-            print(table)
-        except:
-            continue
-    connection.close()
-def date_from_webkit(timestamp):
-    epoch_start = datetime.datetime(1601,1,1)
-    delta = datetime.timedelta(microseconds=int(timestamp))
-    print(epoch_start+delta)
-def ordered_url_history():
-    q = "SELECT * FROM urls ORDER BY last_visit_time DESC;"
-    cursor.execute("SELECT sql from sqlite_schema where name='urls';")
-    print(cursor.fetchall())
-    cursor.execute(q)
-    data =  cursor.fetchall()
-    for line in data:
-        try:
-            date_from_webkit(int(line[5]))
-            print(line,"\n","--"*50)
-        except:
-            continue
+class Parse_History():
+    def __init__(self, db=None):
+        self.db = db
+        if db == None:
+            self.db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
+            
+    def exec_query(self, query):
+        connection = sqlite3.connect(self.db)
+        cursor = connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        connection.close()
+        return data
 
+    def date_from_webkit(self, timestamp):
+        # convert webkit_timestamp to readable format
+        epoch_start = datetime.datetime(1601,1,1)
+        delta = datetime.timedelta(microseconds=int(timestamp))
+        return epoch_start+delta
+    
+    def navigation_history(self):
+        query = "SELECT * FROM urls ORDER BY last_visit_time DESC;"
+        table_data = self.exec_query(query=query)
+        for line in table_data:
+            try:
+                readable_date = self.date_from_webkit(int(line[5]))
+                print(line,"\n","-"*50)
+            except:
+                pass
+    
+    def download_history(self):
+        query = "SELECT * FROM downloads;"
+        data = self.exec_query(query=query)
+        data = str(data)
+        data = data.split(')')
+        for line in data:
+            print(line+"\n")
+            print("-"*200)
+            print("\n")
+    
+    def run_class(self):
+        self.navigation_history()
+        self.download_history()
 
-def visited_links_ordered_by_count():
-    q = "SELECT * FROM sqlite_schema WHERE name='visited_links';"
-    cursor.execute(q)
-    print(cursor.fetchall())
-    q = "SELECT * FROM visited_links ORDER BY visit_count DESC;"
-    cursor.execute(q)
-    for line in cursor.fetchall():
-        try:
-            print(line)
-        except:
-            continue
+parse_obj = Parse_History()
+parse_obj.run_class()
 
-connection.close()
+    
---
+++ chrome_nav_history.py
@@ -0,0 +1,49 @@
+import os
+import sqlite3
+import datetime
+
+db = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/History')
+connection = sqlite3.connect(db)
+cursor = connection.cursor()
+
+def get_tables():
+    cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
+    for table in cursor.fetchall():
+        try:
+            print(table)
+        except:
+            continue
+    connection.close()
+def date_from_webkit(timestamp):
+    epoch_start = datetime.datetime(1601,1,1)
+    delta = datetime.timedelta(microseconds=int(timestamp))
+    print(epoch_start+delta)
+def ordered_url_history():
+    q = "SELECT * FROM urls ORDER BY last_visit_time DESC;"
+    cursor.execute("SELECT sql from sqlite_schema where name='urls';")
+    print(cursor.fetchall())
+    cursor.execute(q)
+    data =  cursor.fetchall()
+    for line in data:
+        try:
+            date_from_webkit(int(line[5]))
+            print(line,"\n","--"*50)
+        except:
+            continue
+
+
+def visited_links_ordered_by_count():
+    q = "SELECT * FROM sqlite_schema WHERE name='visited_links';"
+    cursor.execute(q)
+    print(cursor.fetchall())
+    q = "SELECT * FROM visited_links ORDER BY visit_count DESC;"
+    cursor.execute(q)
+    for line in cursor.fetchall():
+        try:
+            print(line)
+        except:
+            continue
+
+
+
+connection.close()
